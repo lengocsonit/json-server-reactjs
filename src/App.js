2e2444a4f4a19c81df23e490558ba27e2ccfe915
le ngoc son <sonln.hust@gmail.com>
2020-05-07 23:42:56 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class componentName extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sendData = () => {
+      var url = 'http://localhost:3004/posts';
+      var data = {username: 'example', username2: 'example'};
+
+fetch(url, {
+  method: 'POST', // or 'PUT'
+  body: JSON.stringify(data), // data can be `string` or {object}!
+  headers:{
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json())
+.then(response => console.log('Success:', JSON.stringify(response)))
+.catch(error => console.error('Error:', error));
+    }
+
+    
+    this.getData = () => {
+      
+      return fetch('http://localhost:3004/posts')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+    }
+  }
+  
+  render() {
+    return (
+      
+    <div>
+      <button onClick = {this.sendData}>Click</button>
+      <button onClick = {this.getData}>Click</button>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
